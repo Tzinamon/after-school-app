2721372e290f57c252654c9d6653fd777c55f178
Eran Tzinamon <tzinamon@gmail.com>
2021-08-07 01:16:09 +0300

    empty Footer and TopNav added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './components/footer/footer';
+import TopNav from './components/TopNav/TopNav';
 import DashboardPage from './pages/dashboard/DashboardPage';
 import HomePage from './pages/homepage/HomePage';
 import LoginPage from './pages/login/LoginPage';
@@ -12,8 +14,12 @@ function App() {
           <Switch>
             <Route exact path="/"><HomePage/></Route>
             <Route exact path="/login"><LoginPage/></Route>
-            <Route exact path="/dashboard"><DashboardPage/></Route>
+            <Route exact path="/dashboard">
+              <TopNav/>
+              <DashboardPage/>
+            </Route>
           </Switch>
+        <Footer/>
         </HashRouter>
     </div>
   );
