60fdede109487ea13b196fefd759e3599d66fe27
Eran Tzinamon <tzinamon@gmail.com>
2021-08-08 01:50:27 +0300

    created user and classes json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 
+import { useState } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/footer';
 import TopNav from './components/TopNav/TopNav';
+import UserModel from './Model/UserModel';
 import DashboardPage from './pages/dashboard/DashboardPage';
 import HomePage from './pages/homepage/HomePage';
 import LoginPage from './pages/login/LoginPage';
+import jsonUsers from './data/users.json';
+import jsonClasses from './data/classes.json';
 
 function App() {
+  const [users, setUsers] = useState(jsonUsers.map(plainUser => new UseRModel(plainUser)));
+  const [userRole, setUserRole] = useState(new UserModel(
+    {
+      id : "1",
+      fname : "John",
+      lname : "Doe",
+      email : "johndoe@gmail.com",
+      role : "manager",
+      password : "12345"
+  }
+  ));
+
+  // const [userRole, setUserRole] = useState(new UserModel(
+  //   {
+  //     id : "2",
+  //     fname : "Jack",
+  //     lname : "Smith",
+  //     email : "jacksmith@gmail.com",
+  //     role : "parent",
+  //     password : "54321",
+  // }
+  // ));
+
   return (
     <div >
         <HashRouter>
           <Switch>
-            <Route exact path="/"><HomePage/></Route>
+            <Route exact path="/"><HomePage userRole={userRole}/></Route>
             <Route exact path="/login"><LoginPage/></Route>
             <Route exact path="/dashboard">
-              <TopNav/>
-              <DashboardPage/>
+              <TopNav userRole={userRole}/>
+              <DashboardPage userRole={userRole}/>
             </Route>
           </Switch>
         <Footer/>
