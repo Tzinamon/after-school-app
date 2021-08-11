0090097e19d76128d256c53b191020806be89399
Eran Tzinamon <tzinamon@gmail.com>
2021-08-12 02:48:23 +0300

    added footer style and positioning

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./footer.css";
+
 
 function Footer(props) {
     return (
-        <div>
-            Footer
+        <div className= "f-footer">
+            <div>
+                <div>חוגי חגיגה</div>
+                <div>דברו איתנו</div>
+            </div>
+            <div>WhatsApp icon</div>
+            <div>כל הזכויות שמורות</div>
         </div>
     );
 }
